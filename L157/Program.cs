81bf6f808a131b2175fbfbaa25869f1af171cbdc
lekonai <lekonai@users.noreply.github.com>
2024-03-13 14:53:21 +0000

    asdasdasdasd

---
+++ L157/Program.cs
@@ -0,0 +1,86 @@
+﻿namespace L157;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        // string postFixExpr = "1 2 3 - + 4 5 - 6 * 7 8 + + 9 + -";
+        Console.WriteLine("Input RPN Expression: ");
+        string input = Console.ReadLine();
+        string[] sentenceSplit = input.Split(' ');
+        TheStack rpnStack = new TheStack();
+        for (int i = 0; i < sentenceSplit.Length; i++)
+        {
+            if (isInt(sentenceSplit[i]))
+            {
+                int currentNum = Convert.ToInt32(sentenceSplit[i]); 
+                rpnStack.Push(currentNum);
+                Console.WriteLine(currentNum);
+            }
+            else
+            {
+                int val2 = rpnStack.Pop();
+                int val1 = rpnStack.Pop();
+                switch (sentenceSplit[i])
+                {
+                    case "-":
+                    {
+                        rpnStack.Push(sub(val1,val2));
+                        break;
+                    }
+                    case "+":
+                    {
+                        rpnStack.Push(add(val1,val2));
+                        break;
+                    }
+                    case "*":
+                    {
+                        rpnStack.Push(mult(val1,val2));
+                        break;
+                    }
+                    case "/":
+                    {
+                        rpnStack.Push(div(val1,val2));
+                        break;
+                    }
+                }
+            }
+            
+        }
+
+        Console.WriteLine(rpnStack.Seek());
+    }
+
+    static bool isInt(string inp)
+    {
+        if (inp == "0" ||inp == "1" || inp == "2" || inp == "3" ||
+            inp == "4" || inp == "5" || inp == "6" ||
+            inp == "7" || inp == "8" || inp == "9")
+        {
+            return true;
+        }
+        return false;
+    }
+
+    static int sub(int left, int right)
+    {
+        int val = 0;
+        val = left - right;
+        return val;
+    }
+
+    static int add(int left, int right)
+    {
+        return left + right;
+    }
+
+    static int mult(int left, int right)
+    {
+        return left * right;
+    }
+
+    static int div(int left, int right)
+    {
+        return left / right;
+    }
+}
---
+++ L157/TheStack.cs
@@ -0,0 +1,68 @@
+﻿using System.Security.Cryptography;
+
+namespace L157;
+
+public class TheStack
+{
+    private int[] theStack = new int[10];
+    private int pointer = -1;
+
+    public void Push(int input)
+    {
+        
+        if (isFull() == false)
+        {
+            // Console.WriteLine("Put in a value:");
+            // int input = int.Parse(Console.ReadLine());
+            theStack[pointer + 1] = input;
+            pointer++;
+        }
+        else
+        {
+            Console.WriteLine("Unable to push, the stack's full.");
+        }
+    }
+
+    public int Pop()
+    {
+        if (isEmpty())
+        {
+            Console.WriteLine("You can't pop, the stack's empty.");
+            return 0;
+        }
+        else
+        {
+            int popVal = theStack[pointer];
+            pointer--;
+            return popVal;
+        }
+    }
+
+    public int Seek()
+    {
+        if (isEmpty() == false)
+        {
+            return theStack[pointer];   
+        }
+        Console.WriteLine("Stack's empty...");
+        return 0;
+    }
+
+    public bool isEmpty()
+    {
+        if (pointer < 0)
+        {
+            return true;
+        }
+        return false;
+    }
+
+    public bool isFull()
+    {
+        if (pointer == theStack.Length)
+        {
+            return true;
+        } 
+        return false;
+    }
+}
