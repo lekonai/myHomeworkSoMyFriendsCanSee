7f8b170d2723fa3e3dad06af07d2c107620e8f58
lekonai <lekonai@users.noreply.github.com>
2024-06-03 17:49:38 +0100

    ad

---
+++ W137 - Dictionaries/Program.cs
@@ -0,0 +1,29 @@
+﻿using System.Threading.Channels;
+
+namespace W137___Dictionaries;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Dictionary<string, int> wordsReps = new Dictionary<string, int>();
+        string inp = Console.ReadLine();
+        string[] words = inp.Split(" ");
+        for (int i = 0; i < inp.Length; i++)
+        {
+            if (wordsReps.ContainsKey(words[i]))
+            {
+                wordsReps[words[i]]++;
+            }
+            else
+            {
+                wordsReps.Add(words[i], 1);
+            }
+        }
+
+        // foreach (string in COLLECTION)
+        // {
+        //     
+        // }
+    }
+}
