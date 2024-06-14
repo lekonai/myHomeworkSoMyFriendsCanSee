e80f019f126f302861f0e30bfa5ff534d89044f5
lekonai <lekonai@users.noreply.github.com>
2024-06-14 21:36:32 +0100

    too ez done in 5

---
+++ A132/Program.cs
@@ -0,0 +1,31 @@
+﻿namespace A132;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine(uncensor("*h*s *s v*ry *tr*ng*", "Tiiesae"));
+        Console.WriteLine(uncensor("A**Z*N*", "MAIG"));
+        Console.WriteLine(uncensor("xyz", ""));
+    }
+
+    static string uncensor(string n, string fill)
+    {
+        // n = sentence to decode
+        string newString = "";
+        int count = 0;
+        for (int i = 0; i < n.Length; i++)
+        {
+            if (n[i] == '*')
+            {
+                newString += fill[count];
+                count++;
+            }
+            else
+            {
+                newString += n[i];
+            }
+        }
+        return newString;
+    }
+}
