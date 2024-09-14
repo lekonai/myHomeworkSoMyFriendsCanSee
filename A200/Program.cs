0694ebf1715b39ffcc334433f828d46695c43931
lekonai <lekonai@users.noreply.github.com>
2024-09-14 13:49:42 +0100

    A200 complete.

---
+++ A200/Program.cs
@@ -0,0 +1,30 @@
+﻿namespace A200;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        while (true)
+        {
+            string inp = Console.ReadLine();
+            int num = Alphabets(inp);
+            Console.WriteLine(num);
+        }
+    }
+
+    static int Alphabets(string inp)
+    {
+        inp = inp.ToLower(); // amendment lol.
+        int amountOfAlphabetLetters = 0;
+        for (int i = 0; i < inp.Length; i++)
+        {
+            int cCharAlphabetPosition = inp[i] - 96;
+            int cPosition = i + 1;
+            if (cCharAlphabetPosition == cPosition)
+            {
+                amountOfAlphabetLetters++;
+            }
+        }
+        return amountOfAlphabetLetters;
+    }
+}
