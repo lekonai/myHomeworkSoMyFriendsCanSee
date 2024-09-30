f5a6cabf42ce402f86862e84f3b652426fe4405c
lekonai <lekonai@users.noreply.github.com>
2024-09-30 20:50:21 +0100

    ezpz great bubble sort

---
+++ A202/Program.cs
@@ -0,0 +1,35 @@
+﻿namespace A202;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        BubbleSort(); // completed in 7m 40s, but realistically finished the algorithm in about 6m 30s and sorted out where my logic error was after that.
+    }
+
+    static void BubbleSort()
+    {
+        int[] bAr = { 93, 46, 85, 90, 66, 57, 75, 5, 33, 45 };
+        bool sorted;
+        do
+        {
+            sorted = true;
+            for (int i = 0; i < bAr.Length - 1; i++)
+            {
+                int temp1 = bAr[i];
+                int temp2 = bAr[i + 1];
+                if (temp2 < temp1)
+                {
+                    bAr[i] = temp2;
+                    bAr[i + 1] = temp1;
+                    sorted = false;
+                }
+            }
+        } while (sorted == false);
+
+        for (int i = 0; i < bAr.Length; i++)
+        {
+            Console.WriteLine(bAr[i]);
+        }
+    }
+}
