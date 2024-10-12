ad312b4f89ec98c64c028525476feccc142b98e3
lekonai <lekonai@users.noreply.github.com>
2024-10-12 12:57:08 +0100

    done ezpz

---
+++ A204/Program.cs
@@ -0,0 +1,34 @@
+﻿namespace A204;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        string input = "nouveau";
+        Console.WriteLine(VowelSender(input));
+    }
+
+    static string VowelSender(string n)
+    {
+        string newString = "";
+        List<string> ar = new List<string>();
+        foreach (var v in n)
+        {
+            ar.Add(v.ToString());
+        }
+        string addToEnd = "";
+        for (int i = 0; i < ar.Count; i++)
+        {
+            if (ar[i].Contains("a") || ar[i].Contains("e") || ar[i].Contains("i") || ar[i].Contains("o") || ar[i].Contains("u"))
+            {
+                addToEnd += ar[i];
+            }
+            else
+            {
+                newString += ar[i];
+            }
+        }
+        
+        return newString = newString += addToEnd;
+    }
+}
