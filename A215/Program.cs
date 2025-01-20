76501cc8d36db599d7fb18f9f76d129aaf4a7eca
lekonai <lekonai@users.noreply.github.com>
2025-01-20 21:14:34 +0000

    Done.

--- A215/Program.cs
+++ A215/Program.cs
@@ -1,9 +1,32 @@
-﻿amespace A215;
+﻿namespace A215;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello, World!");
+        Console.WriteLine(BPCCDoorperson("llamas"));
+    }
+
+    static int BPCCDoorperson(string str)
+    {
+        bool doubleDetected = false;
+        int numReturned = 0;
+        for (int i = 0; i < str.Length; i++)
+        {
+            if (i < str.Length - 1)
+            {
+                if (str[i] == str[i + 1])
+                {
+                    numReturned = (i + 1) * 3;
+                    doubleDetected = true;
+                }
+            }
+        }
+
+        if (!doubleDetected)
+        {
+            return -1;
+        } 
+        return numReturned;
     }
 }
