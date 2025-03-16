3d5e52e931511d023a3507a17097b3b6237ce92b
lekonai <lekonai@users.noreply.github.com>
2025-03-16 17:26:43 +0000

    done.

---
+++ A222/Program.cs
@@ -0,0 +1,40 @@
+﻿namespace A222;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Dictionary<char, int> necessaryDictionary = ColoursNErrors("wpetrificusutotalusuwuexpectoupatronumuwuwingardiumuleviosauwu");
+        BarChartProducer(necessaryDictionary);
+    }
+
+    static Dictionary<char, int> ColoursNErrors(string str)
+    {
+        Dictionary<char, int> toBeReturned = new Dictionary<char, int>();
+        for (int i = 0; i < str.Length; i++)
+        {
+            if (!toBeReturned.ContainsKey(str[i])) // the only error was here, i forgot the exclamation mark.
+            {
+                toBeReturned.Add(str[i], 1);
+            }
+            else
+            {
+                toBeReturned[str[i]] += 1;
+            }
+        }
+
+        return toBeReturned;
+    }
+
+    static void BarChartProducer(Dictionary<char, int> dict)
+    {
+        char[] errorsArray = new char[] { 'u', 'w', 'x', 'y', 'z' };
+        for (int i = 0; i < errorsArray.Length; i++)
+        {
+            if (dict.ContainsKey(errorsArray[i]))
+            {
+                Console.WriteLine("{0}: {1}", errorsArray[i], dict[errorsArray[i]]);
+            }
+        }
+    }
+}
