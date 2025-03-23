35009ac6052482ae4f7836ba7f6831c9d90411d9
lekonai <lekonai@users.noreply.github.com>
2025-03-23 21:28:07 +0000

    d

---
+++ A223/Program.cs
@@ -0,0 +1,97 @@
+﻿using System.Runtime.CompilerServices;
+using System.Threading.Channels;
+using Microsoft.VisualBasic;
+
+namespace A223;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        int highest = 0;
+        List<string> currList = listOfConsonantSubstring("hufflepuff");
+        foreach (string v in currList)
+        {
+            Console.WriteLine("the substring is {0}, and the value of this is: {1}", v, highestValueConsonantSubstring(v));
+            if (highest < highestValueConsonantSubstring(v))
+            {
+                highest = highestValueConsonantSubstring(v);
+            }
+        }
+
+        Console.WriteLine("that would make the highest substring have a value of {0}", highest);
+    }
+
+    static List<string> listOfConsonantSubstring(string str)
+    {
+        List<string> subStrL = new List<string>();
+        string tempString = "";
+        for (int i = 0; i < str.Length; i++)
+        {
+            switch (str[i]) // bit finnicky in comparison to what i would've tried out (regex), but it is what the question asks for, and i can't deviate from that
+            {
+                case 'a':
+                    subStrL.Add(tempString);
+                    tempString = "";
+                    break;
+                case 'e':
+                    subStrL.Add(tempString);
+                    tempString = "";
+                    break;
+                case 'i':
+                    subStrL.Add(tempString);
+                    tempString = "";
+                    break;
+                case 'o':
+                    subStrL.Add(tempString);
+                    tempString = "";
+                    break;
+                case 'u':
+                    subStrL.Add(tempString);
+                    tempString = "";
+                    break;
+                default:
+                    tempString += str[i];
+                    break;
+            }
+        }
+        subStrL.Add(tempString);
+        return subStrL;
+    }
+
+    static int highestValueConsonantSubstring(string st)
+    {
+        int substringT = 0;
+        for (int j = 0; j < st.Length; j++)
+        {
+            substringT += st[j] - 96;
+        }
+        return substringT;
+    }
+
+
+
+
+
+
+    static string GetMiddle(string str)
+    {
+        if (str.Length == 0)
+        {
+            return "";
+        }
+        if (str.Length % 2 == 0)
+        {
+            // even
+            string evenString = str[(str.Length / 2) - 1].ToString() + str[str.Length / 2];
+            return evenString;
+        }
+        if (str.Length % 2 == 1)
+        {
+            // odd
+            return str[str.Length / 2].ToString();
+        }
+        return "";
+    }
+    
+}
---
+++ ExamP/Program.cs
@@ -0,0 +1,92 @@
+﻿namespace ExamP;
+
+class Program
+{
+    public static void Main(string[] args)
+    {
+        int number;
+
+        do
+        {
+            Console.Write("Enter an integer greater than 0: ");
+            if (int.TryParse(Console.ReadLine(), out number) && number <= 0)
+            {
+                Console.WriteLine("Invalid input. Please enter a number greater than 0.");
+            }
+        } while (number <= 0); // is there a typo on the exam?
+
+        if (IsIncreasing(number))
+        {
+            Console.WriteLine($"{number} is not a bouncy number because it is an increasing number.");
+        }
+        else if (IsDecreasing(number))
+        {
+            Console.WriteLine($"{number} is not a bouncy number because it is a decreasing number.");
+        }
+        else if (IsPerfectlyBouncy(number))
+        {
+            Console.WriteLine($"{number} is a perfectly bouncy number.");
+        }
+        else
+        {
+            Console.WriteLine($"{number} is a bouncy number.");
+        }
+    }
+
+    public static bool IsIncreasing(int number)
+    {
+        string numStr = number.ToString();
+        for (int i = 1; i < numStr.Length; i++)
+        {
+            if (numStr[i] < numStr[i - 1])
+            {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    public static bool IsDecreasing(int number)
+    {
+        string numStr = number.ToString();
+        for (int i = 1; i < numStr.Length; i++)
+        {
+            if (numStr[i] > numStr[i - 1])
+            {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    public static bool IsBouncy(int number)
+    {
+        return !IsIncreasing(number) && !IsDecreasing(number);
+    }
+
+    public static bool IsPerfectlyBouncy(int number)
+    {
+        if (!IsBouncy(number))
+        {
+            return false;
+        }
+
+        string numStr = number.ToString();
+        int largerCount = 0;
+        int smallerCount = 0;
+
+        for (int i = 0; i < numStr.Length - 1; i++)
+        {
+            if (numStr[i] < numStr[i + 1])
+            {
+                largerCount++;
+            }
+            else if (numStr[i] > numStr[i + 1])
+            {
+                smallerCount++;
+            }
+        }
+
+        return largerCount == smallerCount;
+    }
+}
